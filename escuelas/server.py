f8a423492f4f6662dacf0eb0d0e0b8c289baefb9
Maria Guadalupe Ayala Bernal <maria.ayala3753@alumnos.udg.mx>
2019-12-07 00:10:47 -0600

    Conexión a la base de datos

--- escuelas/server.py
+++ escuelas/server.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+# MySQL configurations
+app.config['MYSQL_USER'] = 'WCqgiEsLy3'
+app.config['MYSQL_PASSWORD'] = 'I9yyxDyDIJ'
+app.config['MYSQL_DB'] = 'WCqgiEsLy3'
+app.config['MYSQL_HOST'] = 'remotemysql.com'
+
+mysql = MySQL(app)
+
 @app.route('/')
 def inicia():
-    return "Hello World"
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT `domicilio`, `municipio` FROM `primaria`")
+    resultsPrimaria = cursor.fetchall()
+    print(resultsPrimaria)
+    cursor.execute("SELECT `domicilio`, `municipio` FROM `secundaria`")
+    resultsSecundaria = cursor.fetchall()
+    print(resultsSecundaria)
+    cursor.execute("SELECT `domicilio`, `municipio` FROM `preparatoria`")
+    resultsPreparatoria = cursor.fetchall()
+    print(resultsPreparatoria)
+    return 'Done!'
 
 @app.route('/mapa')
 def verMapa():
@@ -12,3 +31,4 @@ def verMapa():
 
 if __name__ == "__main__":
     app.run()
+    
