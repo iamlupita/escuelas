dbb99cb348774eb55b6d2c4bbe651ccfc1d6f0bb
Maria Guadalupe Ayala Bernal <maria.ayala3753@alumnos.udg.mx>
2019-12-08 01:11:53 -0600

    latitud y longitud con error

--- escuelas/server.py
+++ escuelas/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_mysqldb import MySQL
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
 
 app = Flask(__name__)
 
@@ -11,23 +13,39 @@ app.config['MYSQL_HOST'] = 'remotemysql.com'
 
 mysql = MySQL(app)
 
-@app.route('/')
-def inicia():
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT `domicilio`, `municipio` FROM `primaria`")
-    resultsPrimaria = cursor.fetchall()
-    print(resultsPrimaria)
-    cursor.execute("SELECT `domicilio`, `municipio` FROM `secundaria`")
-    resultsSecundaria = cursor.fetchall()
-    print(resultsSecundaria)
-    cursor.execute("SELECT `domicilio`, `municipio` FROM `preparatoria`")
-    resultsPreparatoria = cursor.fetchall()
-    print(resultsPreparatoria)
-    return 'Done!'
+nom=Nominatim()
 
-@app.route('/mapa')
+def do_geocode(address):
+    try:
+        return nom.geocode(address)
+    except GeocoderTimedOut:
+        return do_geocode(address)
+
+@app.route('/')
 def verMapa():
-    return render_template("mapa.html")
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT `nom_escuela`,`domicilio`, `municipio` FROM `primaria`")
+    resultsPrimaria = cursor.fetchall()
+    for primaria in resultsPrimaria:
+        address=primaria[1]+" "+primaria[2]
+        """coordPrim=do_geocode(address)
+        if(coordPrim):
+            print(coordPrim.latitude,coordPrim.longitude)"""
+    cursor.execute("SELECT `nombre`,`domicilio`, `municipio` FROM `secundaria`")
+    resultsSecundaria = cursor.fetchall()
+    for secundaria in resultsSecundaria:
+        address=secundaria[1]+" "+secundaria[2]
+        """coordSec=do_geocode(address)
+        if(coordSec):
+            print(coordSec.latitude,coordSec.longitude)"""
+    cursor.execute("SELECT `nombre_escuela`,`domicilio`, `municipio` FROM `preparatoria`")
+    resultsPreparatoria = cursor.fetchall()
+    for preparatoria in resultsPreparatoria:
+        address=preparatoria[1]+" "+preparatoria[2]
+        """coordPrep=do_geocode(address)
+        if(coordSec):
+            print(coordPrep.latitude,coordPrep.longitude)"""
+    return render_template("mapa.html",primarias=resultsPrimaria, secundarias=resultsSecundaria, preparatorias=resultsPreparatoria)
 
 if __name__ == "__main__":
     app.run()
